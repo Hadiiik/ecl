55ad6c74e8dfbe8feecca5f63596c44326fc106a
hadiikaddour <hadyqdwr77@gmail.com>
2024-05-11 23:13:08 +0300

    typos

--- app/dashboard/files/components/TextArea.tsx
+++ app/dashboard/files/components/TextArea.tsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React from 'react';
 
-const TextArea = ({onChange}:any) => {
-  const handelChange = (e)=>{
-    onChange(e)
-  }
-  return (
-    <div >
-      
-<label className="block mb-2 text-sm font-medium text-white">وصف الملف </label>
-<textarea id="message" rows={2} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="كلمات مفتاحية و وصف الملف"
-  onChange={handelChange}
-></textarea>
-
-    </div>
-  )
+interface TextAreaProps {
+  onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void; // تحديد نوع المعامل e
 }
 
-export default TextArea
+const TextArea = ({ onChange }: TextAreaProps) => {
+  const handelChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    onChange(e);
+  };
+
+  return (
+    <div>
+      <label className="block mb-2 text-sm font-medium text-white">وصف الملف</label>
+      <textarea
+        id="message"
+        rows={2}
+        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+        placeholder="كلمات مفتاحية و وصف الملف"
+        onChange={handelChange}
+      ></textarea>
+    </div>
+  );
+};
+
+export default TextArea;
