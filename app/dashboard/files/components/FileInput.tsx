4e98ee1c7fda3a919d2debf9ebc0094d949a879c
hadiikaddour <hadyqdwr77@gmail.com>
2024-05-11 23:09:03 +0300

    fixing file input typeos

--- app/dashboard/files/components/FileInput.tsx
+++ app/dashboard/files/components/FileInput.tsx
@@ -1,19 +1,15 @@
 import React from 'react'
 
-const FileInput = ({fileInputRef}) => {
-  return (
-    <div className=' flex  items-center justify-center pt-4 w-full py-2'>
-      <input
-        ref={fileInputRef}
-         type="file" multiple
-        className="flex justify-end   text-sm  text-slate-700 bg-blue-300 rounded-md
-        file:mr-4 file:py-2 file:px-4 file:rounded-md
-        file:border-0 file:text-sm file:font-semibold
-        file:bg-blue-500 file:text-blue-100
-        hover:file:bg-blue-600 cursor-pointer"
-        />
-    </div>
-  )
+interface FileInputProps {
+  fileInputRef: React.RefObject<HTMLInputElement>; // تحديد نوع البيانات الخاص بـ fileInputRef
 }
 
-export default FileInput
+const FileInput = ({ fileInputRef }: FileInputProps) => {
+  return (
+    <div className=' flex  items-center justify-center pt-4 w-full py-2'>
+      <input type='file' ref={fileInputRef} className='hidden' />
+    </div>
+  );
+}
+
+export default FileInput;
